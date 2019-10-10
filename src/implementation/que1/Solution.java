4ea585f0e185ac3f60df564b0f03b74d17b5f666
divaibhav <divaibhav@gmail.com>
2019-10-10 10:37:23 +0530

    created implementation.que1.Solution

---
+++ src/implementation/que1/Solution.java
@@ -0,0 +1,15 @@
+package implementation.que1;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner sc =new Scanner(System.in);
+
+        System.out.println("enter a string");
+
+        String inputString = sc.nextLine();
+
+    }
+}
