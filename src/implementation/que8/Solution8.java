bfb99cd9b5f66c45ee8019e2e036f12d2dca3ee0
divaibhav <divaibhav@gmail.com>
2019-10-19 10:35:31 +0530

    changes in Solution8

--- src/implementation/que8/Solution8.java
+++ src/implementation/que8/Solution8.java
@@ -7,5 +7,13 @@ public class Solution8 {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter a string");
         String inputString = sc.nextLine();
+        int upperWeight = 0;
+        for (int i = 0; i < inputString.length(); i++) {
+            char charFromInputSting = inputString.charAt(i);
+            if(Character.isUpperCase(charFromInputSting)){
+                upperWeight = upperWeight + charFromInputSting;
+            }
+        }
+        System.out.println("Upper Weight = " + upperWeight);
     }
 }
