fb7eb7392c4bbb30d63a2bb151b8c1ac343e46e3
divaibhav <divaibhav@gmail.com>
2019-10-19 17:30:49 +0530

    created and implemented que6.Solution6

---
+++ src/implementation/que6/Solution6.java
@@ -0,0 +1,14 @@
+package implementation.que6;
+
+import java.util.Scanner;
+
+public class Solution6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a string");
+        String inputString = sc.nextLine();
+
+        inputString = inputString.toUpperCase();
+        System.out.println(inputString);
+    }
+}
